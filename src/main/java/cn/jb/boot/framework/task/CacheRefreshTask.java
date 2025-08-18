76f3f1aabad88dc76f6545523dfc34ec08a1ca98
zgr <zhangguorenmi@163.com>
2025-08-18 11:12:56 +0800

    部署V3.7366： 修改procedureList.vue 的设备从设备台账接口来  。优化字典同步

--- src/main/java/cn/jb/boot/framework/task/CacheRefreshTask.java
+++ src/main/java/cn/jb/boot/framework/task/CacheRefreshTask.java
@@ -46,11 +46,18 @@ public class CacheRefreshTask implements CommandLineRunner {
     private DeviceInfoMapper deviceInfoMapper;
 
 
-    @Scheduled(cron = "0 0/60 * * * ?")
+    @Scheduled(cron = "0 0/13 * * * ?")
     public void initData() {
 //        log.info("定时刷新缓存  暂时停止  请手动打开");
-        System.out.println("info:  定时刷新缓存开始  装载字典（频率60分钟）  ");
+        System.out.println("info:  定时刷新缓存开始  装载bom（频率13分钟）  ");
         run();
+    }
+
+    @Scheduled(cron = "0 0/3 * * * ?")
+    public void initData3() {
+//        log.info("定时刷新缓存  暂时停止  请手动打开");
+        System.out.println("info:  定时刷新缓存开始  装载基础字典（频率3分钟）  ");
+        runBom();
     }
 
     @Override
@@ -59,25 +66,59 @@ public class CacheRefreshTask implements CommandLineRunner {
     }
 
 
-    public void run() {
-        log.info("Cache load start：装载基础数据");
+    public void runBom() {
+        // [新增注释] 记录开始装载缓存
+        log.info("Cache load start：装载基础bom数据");
+
+        // [新增注释] 1. 查询所有系统字典并按字典类型分组
         List<DictData> dictData = dictDataService.allDict();
         List<DictDataVo> list = PojoUtil.copyList(dictData, DictDataVo.class);
-        Map<String, List<DictDataVo>> dict = list.stream().collect(Collectors.groupingBy(DictDataVo::getDictType));
-        //加在物料信息表
+        Map<String, List<DictDataVo>> dict =
+                list.stream().collect(Collectors.groupingBy(DictDataVo::getDictType));
+
+//         [新增注释] 2. 加载物料（BOM）信息
         List<DictDataVo> misList = stockMapper.getCache();
         dict.put(DictType.BOM_NO.getCode(), misList);
+
+        // [新增注释] 将所有基础数据写入缓存
+        DictUtil.setDictCache(dict);
+
+        // [新增注释] 记录结束
+        log.info("Cache load start：装载用户数据资源结束………………");
+    }
+
+    public void run() {
+        // [新增注释] 记录开始装载缓存
+        log.info("Cache load start：装载基础数据");
+
+        // [新增注释] 1. 查询所有系统字典并按字典类型分组
+        List<DictData> dictData = dictDataService.allDict();
+        List<DictDataVo> list = PojoUtil.copyList(dictData, DictDataVo.class);
+        Map<String, List<DictDataVo>> dict =
+                list.stream().collect(Collectors.groupingBy(DictDataVo::getDictType));
+
+        // [新增注释] 3. 加载用户信息
         List<DictDataVo> userList = userInfoMapper.getCache();
         dict.put(DictType.USER_INFO.getCode(), userList);
+
+        // [新增注释] 4. 加载车间信息
         List<DictDataVo> workShopList = deptInfoMapper.workShopSelect();
         dict.put(DictType.WORK_SHOP.getCode(), workShopList);
-        List<DictDataVo> locations = warehouseInfoMapper.selected();
-        dict.put(DictType.WARE_HOUSE.getCode(), locations);
+
+        // [新增注释] 5. 加载仓库信息
+//        List<DictDataVo> locations = warehouseInfoMapper.selected();
+//        dict.put(DictType.WARE_HOUSE.getCode(), locations);
+
+        // [新增注释] 6. 加载设备信息
         List<DictDataVo> deviceList = deviceInfoMapper.selected();
         dict.put(DictType.DEVICE.getCode(), deviceList);
 
+        // [新增注释] 将所有基础数据写入缓存
         DictUtil.setDictCache(dict);
 
+        // [新增注释] 记录结束
         log.info("Cache load start：装载用户数据资源结束………………");
     }
+
+
 }
